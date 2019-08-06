c311c9ecd31f9ccb6ded805409cbe00254e1541b
Atil Samancioglu <samancioglu.atil@gmail.com>
2019-08-06 20:52:04 +0300

    Initial Commit

---
+++ MachineLearningImageRecognition/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MachineLearningImageRecognition
+//
+//  Created by Atil Samancioglu on 6.08.2019.
+//  Copyright © 2019 Atil Samancioglu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
